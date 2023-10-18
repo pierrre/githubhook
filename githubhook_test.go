388ffd9c534a969027110e46bf62a07d6fd366de
Pierre Durand <pierredurand@gmail.com>
2023-10-18 18:19:56 +0200

    stop using assert extensions in tests

--- githubhook_test.go
+++ githubhook_test.go
@@ -16,16 +16,7 @@ import (
 	"testing"
 
 	"github.com/pierrre/assert"
-	"github.com/pierrre/assert/ext/pierrrecompare"
-	"github.com/pierrre/assert/ext/pierrreerrors"
-	"github.com/pierrre/assert/ext/pierrrepretty"
 )
-
-func init() {
-	pierrrecompare.Configure()
-	pierrrepretty.ConfigureDefault()
-	pierrreerrors.Configure()
-}
 
 var testRawPayload = []byte(`{"foo":"bar"}`)
 
